55267012187f9879cb31ec6f77f349ce51a897a3
Andrew Walker <andrew@almostawake.com>
2024-01-23 07:18:43 +0000

    working, fine tuning

--- .devcontainer/start.sh
+++ .devcontainer/start.sh
@@ -1,8 +1,8 @@
-echo "starting mongod in background.."
+echo "starting mongod in background.." 2>&1
 sudo mongod &
 
-echo "running npm install"
+echo "running npm install" 2>&1
 npm install
 
-echo "starting node server here, ^C to stop it manually"
-node server
+echo "starting node server here, ^C to stop it manually" 2>&1
+node server 2>&1
